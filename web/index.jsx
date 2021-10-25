eaefd940a791e46690ab758e747712d581e0507d
danielv <danielv@danielv.no>
2021-10-25 23:17:30 +0200

    Working edge_transports grid generation
    Signed-off-by: Daniel <danielv@danielv.no>

--- web/index.jsx
+++ web/index.jsx
@@ -5,7 +5,7 @@ import { PageLayout, ControlContext } from "@clusterio/web_ui";
 import info from "../info";
 
 import "./index.css";
-import { Form, Input, Button, Select, InputNumber } from 'antd';
+import { Form, Input, Button, Select, InputNumber, Checkbox } from 'antd';
 const { Option } = Select;
 
 const layout = {
@@ -18,14 +18,18 @@ const tailLayout = {
 
 function OverviewPage() {
 	let control = useContext(ControlContext);
+	let [loading, setLoading] = useState();
 
-	const onFinish = (values) => {
+	const onFinish = async (values) => {
 		console.log('Success:', values);
-		info.messages.create.send(control, values)
+		setLoading(true);
+		await info.messages.create.send(control, values)
+		setLoading(false);
 	};
 
 	const onFinishFailed = (errorInfo) => {
 		console.log('Failed:', errorInfo);
+		setLoading(false);
 	};
 
 	return <PageLayout nav={[{ name: "Gridworld" }]}>
@@ -33,11 +37,17 @@ function OverviewPage() {
 		<Form
 			name="basic"
 			{...layout}
-			initialValues={{ x_size: 500, y_size: 500, x_count: 2, y_count: 2 }}
+			initialValues={{ name_prefix: "Gridworld", use_edge_transports: true, x_size: 500, y_size: 500, x_count: 2, y_count: 2 }}
 			onFinish={onFinish}
 			onFinishFailed={onFinishFailed}
 			autoComplete="off"
 		>
+			<Form.Item name="name_prefix" label="Instance name prefix" rules={[{ type: 'string' }]}>
+				<Input />
+			</Form.Item>
+			<Form.Item name="use_edge_transports" label="Use edge transports" valuePropName="checked">
+				<Checkbox />
+			</Form.Item>
 			<Form.Item name={"x_size"} label="World size along X axis" rules={[{ type: 'number', min: 0, max: 10000 }]}>
 				<InputNumber />
 			</Form.Item>
@@ -52,7 +62,7 @@ function OverviewPage() {
 			</Form.Item>
 
 			<Form.Item {...tailLayout}>
-				<Button type="primary" htmlType="submit">
+				<Button type="primary" htmlType="submit" loading={loading}>
 					Create gridworld
 				</Button>
 			</Form.Item>
