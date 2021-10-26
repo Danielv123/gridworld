3248c373322802b44d77feeb8a0d1c6f68b2f561
danielv <danielv@danielv.no>
2021-10-26 22:58:36 +0200

    Redone coordinates. Prepare maps with border
    Signed-off-by: Daniel <danielv@danielv.no>

--- instance.js
+++ instance.js
@@ -8,7 +8,13 @@ const libLuaTools = require("@clusterio/lib/lua_tools");
 
 class InstancePlugin extends libPlugin.BaseInstancePlugin {
 	async init() {
+		this.pendingTasks = new Set();
 		this.disconnecting = false;
+	}
+
+	async onStop() {
+		clearInterval(this.pingId);
+		await Promise.all(this.pendingTasks);
 	}
 
 	onPrepareMasterDisconnect() {
@@ -26,6 +32,23 @@ class InstancePlugin extends libPlugin.BaseInstancePlugin {
 				err => this.logger.error(`Unexpected error:\n${err.stack}`)
 			);
 		}
+	}
+
+	async setupWorldRequestHandler(message) {
+		if (this.instance.status !== "running") {
+			return;
+		}
+		let data = message.data;
+
+		let task = this.sendRcon(`/c gridworld.create_spawn("${data.x_size}","${data.y_size}","${data.world_x}","${data.world_y}")`, true);
+		let task2 = this.sendRcon(`/c gridworld.create_world_limit("${data.x_size}","${data.y_size}","${data.world_x}","${data.world_y}")`, true);
+		this.pendingTasks.add(task);
+		this.pendingTasks.add(task2);
+		task.finally(() => { this.pendingTasks.delete(task); });
+		task2.finally(() => { this.pendingTasks.delete(task2); });
+		await task;
+		await task2;
+		return
 	}
 }
 
