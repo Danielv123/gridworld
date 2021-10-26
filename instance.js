646c237a4c0afecfa78939929046de0b4e8bb0ca
danielv <danielv@danielv.no>
2021-10-27 01:19:46 +0200

    Movement and build restrictions added
    Signed-off-by: Daniel <danielv@danielv.no>

--- instance.js
+++ instance.js
@@ -12,6 +12,17 @@ class InstancePlugin extends libPlugin.BaseInstancePlugin {
 		this.disconnecting = false;
 	}
 
+	async onStart() {
+		let data = {
+			x_size: this.instance.config.get("gridworld.grid_x_size"),
+			y_size: this.instance.config.get("gridworld.grid_y_size"),
+			world_x : this.instance.config.get("gridworld.grid_x_position"),
+			world_y : this.instance.config.get("gridworld.grid_y_position"),
+		}
+		await this.sendRcon(`/c gridworld.create_spawn("${data.x_size}","${data.y_size}","${data.world_x}","${data.world_y}", false)`, true);
+		await this.sendRcon(`/c gridworld.create_world_limit("${data.x_size}","${data.y_size}","${data.world_x}","${data.world_y}", false)`, true);
+	}
+
 	async onStop() {
 		clearInterval(this.pingId);
 		await Promise.all(this.pendingTasks);
@@ -32,6 +43,12 @@ class InstancePlugin extends libPlugin.BaseInstancePlugin {
 				err => this.logger.error(`Unexpected error:\n${err.stack}`)
 			);
 		}
+	}
+
+	async runTask(task) {
+		this.pendingTasks.add(task);
+		task.finally(() => { this.pendingTasks.delete(task); });
+		return task
 	}
 
 	async setupWorldRequestHandler(message) {
