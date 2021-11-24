e0d102164f0fe8bc94a36fd1d324de6c8a208ed8
Danielv123 <danielv@danielv.no>
2021-11-24 22:32:38 +0100

    Created modal for reset tiles with progress bar
    https://i.imgur.com/Scfi85W.png
    Progress bar looks nice.
    Added warning about it taking a while
    Hide button depending on account permission

---
+++ web/components/RefreshTileDataButton.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Tooltip, Progress, Modal, Button } from "antd";
+
+import { ControlContext, PageLayout, useInstanceList, useAccount, notifyErrorHandler } from "@clusterio/web_ui";
+import info from "../../info";
+import RefreshTileDataModal from "./RefreshTileDataModal";
+
+export default function RefreshTileDataButton() {
+	const account = useAccount();
+	const [modal, showModal] = React.useState(false);
+	return [
+		account.hasPermission("gridworld.map.refresh") ? <Button
+			key="refreshTileData"
+			onClick={() => showModal(true)}
+		>
+			Refresh tiles
+		</Button> : "",
+		<RefreshTileDataModal
+			key="modal"
+			visible={modal}
+			hideModal={() => showModal(false)}
+		/>,
+	];
+}
