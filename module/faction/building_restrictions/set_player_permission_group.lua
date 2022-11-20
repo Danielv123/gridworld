8be6dacc3532be0ec11ab2b5664339be3d43e62c
Daniel <danielv@danielv.no>
2022-11-20 22:58:28 +0000

    Assign permission groups depending on server owner
    Using built in factorio permissions. There are 3 hardcoded groups:
    * Owner - For the faction owner, normal freeplay
    * Guest - For anyone else, can walk around but not grief
    * Admin - Full access for cluster admins

---
+++ module/faction/building_restrictions/set_player_permission_group.lua
@@ -0,0 +1,21 @@
+local get_player_faction = require("modules/gridworld/faction/get_player_faction")
+
+local function set_player_permission_group(player)
+	if player.admin then
+		player.permission_group = game.permissions.get_group("Admin")
+		player.print("Setting permission group to Admin")
+		return
+	end
+	-- Get claiming faction
+	local claiming_faction_id = global.gridworld.claiming_faction.faction_id
+	local faction = get_player_faction(player)
+	if claiming_faction_id == faction.faction_id then
+		player.permission_group = game.permissions.get_group("Owner")
+		player.print("Setting permission group to Owner")
+	else
+		player.permission_group = game.permissions.get_group("Guest")
+		player.print("Setting permission group to Guest")
+	end
+end
+
+return set_player_permission_group
