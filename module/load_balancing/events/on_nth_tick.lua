3494e51363a4127a7aa22456fae6c69efc28d7fa
Danielv123 <danielv@danielv.no>
2024-04-27 20:30:32 +0200

    Disable load balancing

--- module/load_balancing/events/on_nth_tick.lua
+++ module/load_balancing/events/on_nth_tick.lua
@@ -2,7 +2,8 @@ local clusterio_api = require("modules/clusterio/api")
 
 local function on_nth_tick()
 	-- If the server has been inactive for a while and no players are connected, stop it to conserve resources
-	if next(game.connected_players) == nil and game.tick - global.gridworld.load_balancing.inactivity_shutdown_ticks > global.gridworld.load_balancing.last_activity_tick then
+	-- TODO: Disabled load balancing as it is not yet configurable
+	if false and next(game.connected_players) == nil and game.tick - global.gridworld.load_balancing.inactivity_shutdown_ticks > global.gridworld.load_balancing.last_activity_tick then
 		clusterio_api.send_json("gridworld:load_balancing", {
 			action = "stop_server",
 			load_factor = global.gridworld.load_balancing.load_factor,
